b577a24219a1964ac1b2bcfc06d5acd0050acd70
miruka <miruka@disroot.org>
2018-10-02 06:12:19 -0400

    Refactor some Image parts for speed and pickle
    Use only a single temp file when needing to show the image for kitty
    that will be converted on the fly.
    
    Write temp PNGs without compression since they are deleted as soon as
    kitty is done displaying them.
    
    Only have one "Image.origin" variable, be it an URL, Path or PIL Image.
    
    "location" __init__ argument becomes "source", and accepts:
    path string, URL string, Path object, PIL Image object.
    
    30-35% of speed up measured, and class should hopefully be pickleable
    for multiprocessing now.

--- pixcat/image.py
+++ pixcat/image.py
@@ -1,122 +1,84 @@
 # Copyright 2018 miruka
 # This file is part of pixcat, licensed under LGPLv3.
 
+import io
 import math
 import random
 import re
-from mimetypes import guess_extension, guess_type
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Dict, Generator, List, Optional, Tuple, Union
+from typing import Dict, Generator, Optional, Tuple, Union
 
-from dataclasses import InitVar, dataclass, field
 from PIL import Image as PILImage
+from dataclasses import InitVar, dataclass, field
 
 import requests
 
 from .terminal import TERM
-from .__about__ import __pkg_name__
-from .errors import BadFileError
-
-NET_SESSION = requests.Session()
-
-PNG_TMP_COMPRESS = 3
 
 
 @dataclass
 class Image:
-    location: InitVar[Union[str, Path]]
-    id:       Optional[int] = None
+    source: InitVar[Union[str, Path, PILImage.Image]]
+    id:     Optional[int] = None
 
+    origin: Union[str, Path, PILImage.Image] = field(init=False, default=None)
 
-    path:            Path           = field(init=False, default=None)
-    origin_path:     Optional[Path] = field(init=False, default=None)
-    origin_url:      Optional[str]  = field(init=False, default=None)
-    downloaded_path: Optional[str]  = field(init=False, default=None)
-
-    w: int = field(init=False, default=0)
-    h: int = field(init=False, default=0)
-
-
-    _pil_image: type(PILImage) = field(init=False, repr=False, default=None)
-
-    _tmpfile_keepalive: List[type(NamedTemporaryFile)] = \
-        field(init=False, repr=False, compare=False, default_factory=list)
+    _pil_image: PILImage.Image = field(init=False, repr=False, default=None)
 
     _resized_cache: Dict[Tuple[str, tuple], "Image"] = \
         field(init=False, repr=False, compare=False, default_factory=dict)
 
 
-    def __post_init__(self, location) -> None:
-        # To make pylint shut up
-        self._resized_cache     = {}
-        self._tmpfile_keepalive = []
+    def __post_init__(self, source) -> None:
+        self._resized_cache = {}  # to make pylint shut up
 
-        if re.match(r"https?://.+", str(location)):
-            self._get_from_url(location)
-        else:
-            self.path        = Path(location).expanduser().resolve()
-            self.origin_path = self.path
-
-        self._process_input_file()
-        self._pil_image = PILImage.open(self.path)
-        self.w, self.h  = self._pil_image.size
-
-        # TODO: verify available
-        self.id = self.id or random.randint(1, 4_294_967_295)
+        self.id         = self.id or random.randint(1, 4_294_967_295)
+        self.origin     = source
+        self._pil_image = self._get_pil_image(source)
 
 
-    def _get_from_url(self, url: str) -> Path:
-        response = NET_SESSION.get(url)
+    def _get_pil_image(self, source) -> PILImage.Image:
+        # PILImage: just return
 
-        # Raise exception if HTTP return code is between 400 and 600.
-        response.raise_for_status()
+        if isinstance(source, PILImage.Image):
+            return source
 
-        ext  = guess_extension(response.headers["Content-Type"])
-        file = NamedTemporaryFile(prefix=f".{__pkg_name__}-", suffix=ext)
-        file.write(response.content)
+        # URL: Fetch content, make and return Image
 
-        self.path       = self.downloaded_path = Path(file.name)
-        self.origin_url = url
+        if re.match(r"https?://.+", str(source)):
+            response = requests.get(source)
 
-        self._tmpfile_keepalive.append(file)
+            # Raise exception if HTTP return code is between 400 and 600.
+            response.raise_for_status()
 
+            with io.BytesIO() as out:
+                out.write(response.content)
+                out.seek(0)
+                return PILImage.open(out)
 
-    def _process_input_file(self) -> None:
-        if not self.path.is_file():
-            raise BadFileError(self, f"doesn't exist or not a file.")
+        # File path: return Image at path
 
-        mime, encoding = guess_type(str(self.path))
-
-        if encoding is not None:
-            raise BadFileError(self, f"unsupported encoding: {encoding}")
-
-        if not mime or not mime.startswith("image/"):
-            raise BadFileError(self, "file was not recognized as image.")
-
-        if mime == "image/png":
-            return
-
-        self.path = self._get_png()
+        self.origin = path = Path(source).expanduser().resolve()
+        return PILImage.open(path)
 
 
-    def _get_png(self) -> Path:
-        file = NamedTemporaryFile(prefix=f".{__pkg_name__}-", suffix=".png")
+    def _get_kitty_file(self) -> str:
+        image = self._pil_image
+        dest  = NamedTemporaryFile(prefix=".pixcat-", delete=False)
 
-        PILImage.open(self.path).save(file.name,
-                                      compress_level=PNG_TMP_COMPRESS)
-        self._tmpfile_keepalive.append(file)
+        image.save(dest.name, format="PNG", compress_level=0)
 
-        return Path(file.name)
+        return dest.name
 
 
     @property
-    def cols(self):
-        return math.ceil(self.w / TERM.cell_px_width)
+    def cols(self) -> int:
+        return math.ceil(self._pil_image.size[0] / TERM.cell_px_width)
 
     @property
-    def rows(self):
-        return math.ceil(self.h / TERM.cell_px_height)
+    def rows(self) -> int:
+        return math.ceil(self._pil_image.size[1] / TERM.cell_px_height)
 
 
     @staticmethod
@@ -136,13 +98,13 @@ class Image:
                stretch:  bool          = False,
                resample: str           = "lanczos") -> "Image":
 
-        max_w = max_w or self.w
-        max_h = max_h or self.h
+        w, h = img_w, img_h = self._pil_image.size
+
+        max_w = max_w or img_w
+        max_h = max_h or img_h
 
         assert min_w <= max_w
         assert min_h <= max_h
-
-        w, h = self.w, self.h
 
         min_w = self._negative_col_to_px(min_w)
         min_h = self._negative_row_to_px(min_h)
@@ -150,34 +112,33 @@ class Image:
         max_h = self._negative_row_to_px(max_h)
 
         # Upscale if image is smaller than minimum width/height:
-        if (self.w < min_w or  self.h < min_h) and \
-           self.w < max_w and self.h < max_h:
+        if (img_w < min_w or img_h < min_h) and img_w < max_w and img_h <max_h:
 
             if stretch:
                 w, h = min_w, min_h
 
             elif min_w >= min_h:
                 # If calculated height > max_h: max_h, if < min_h: min_h
-                h = min(max_h, math.ceil((min_w / self.w) * self.h))
-                w = math.floor((h / self.h) * self.w)
+                h = min(max_h, math.ceil((min_w / img_w) * img_h))
+                w = math.floor((h / img_h) * img_w)
 
             else:
-                w = min(max_w, math.ceil((min_h / self.h) * self.w))
-                h = math.floor((w / self.w) * self.h)
+                w = min(max_w, math.ceil((min_h / img_h) * img_w))
+                h = math.floor((w / img_w) * img_h)
 
         # Downscale if image is bigger than maximum width/height:
-        elif self.w > max_w or self.h > max_h:
+        elif img_w > max_w or img_h > max_h:
 
             if stretch:
                 w, h = max_w, max_h
 
             elif max_w >= max_h:
-                h = min(max_h, math.ceil((max_w / self.w) * self.h))
-                w = math.floor((h / self.h) * self.w)
+                h = min(max_h, math.ceil((max_w / img_w) * img_h))
+                w = math.floor((h / img_h) * img_w)
 
             else:
-                w = min(max_w, math.ceil((max_h / self.h) * self.w))
-                h = math.floor((w / self.w) * self.h)
+                w = min(max_w, math.ceil((max_h / img_h) * img_w))
+                h = math.floor((w / img_w) * img_h)
 
         # Nothing to do:
         else:
@@ -189,27 +150,13 @@ class Image:
         if cached:
             return cached
 
-        # Resize image and save to temporary file:
+        # Return and save in the cache dict an Image object of the resized.
 
-        filt = getattr(PILImage, resample.upper())
-        img  = self._pil_image.resize((w, h), filt)
-        file = NamedTemporaryFile(prefix=f".{__pkg_name__}-", suffix=".png")
-        img.save(file.name, compress_level=PNG_TMP_COMPRESS)
+        resample = getattr(PILImage, resample.upper())
+        image    = type(self)(self._pil_image.resize((w, h), resample))
 
-        # New Image object of the resized image, bind temp file handle to it.
-        # Save it in the cache dict so it can be re-used later.
-        # The temp file will exist as long as the Image is in the cache dict.
-
-        img                  = Image(file.name)
-        img.origin_path      = self.origin_path
-        img.origin_url       = self.origin_url
-        img.downloaded_path  = self.downloaded_path
-
-        # pylint: disable=protected-access
-        img._tmpfile_keepalive.append(file)
-        self._resized_cache[(w, h)] = img
-
-        return img
+        self._resized_cache[(w, h)] = image
+        return image
 
 
     def thumbnail(self,
@@ -263,10 +210,10 @@ class Image:
             "z_index":  z,
 
             "action": "transmit+display",
-            "medium" : "file",
+            "medium" : "tempfile",
             "format" : "png",
             "id":      self.id,
-            "payload": str(self.path),
+            "payload": self._get_kitty_file(),
         }
 
         if align == "center":
@@ -289,19 +236,20 @@ class Image:
         return self
 
 
-    def hide(self) -> "Image":
-        TERM.run_code(action="delete", del_data_target="id", id=self.id)
+    def hide(self, resized_too: bool = True) -> "Image":
+        ids = [self.id]
+
+        if resized_too:
+            ids += [img.id for img in self._resized_cache.values()]
+
+        for id_ in ids:
+            TERM.run_code(action="delete", del_data_target="id", id=id_)
+
         return self
 
 
     def copy(self, new_id: Optional[int] = None) -> "Image":
-        obj = type(self)(self.path, id=new_id)
-
-        obj.origin_path      = self.origin_path
-        obj.origin_url       = self.origin_url
-        obj.downloaded_path  = self.downloaded_path
-
-        return obj
+        return type(self)(source=self.origin, id=new_id)
 
 
     def __copy__(self) -> "Image":
@@ -309,31 +257,27 @@ class Image:
 
 
     @classmethod
-    def factory(cls, *locations: Union[Path, str]
-               ) -> Generator["Image", None, None]:
+    def factory(cls,
+                *sources:     Union[Path, str],
+                ignore_fails: bool = True,
+                print_fails:  bool = False) -> Generator["Image", None, None]:
 
-        for location in locations:
-            # URL
-            if re.match(r"https?://.+", str(location)):
-                try:
-                    yield cls(location)
-                except (BadFileError, requests.RequestException) as err:
-                    print(TERM.red(f"\n{err.args[0]}\n"))
-                continue
+        for source in sources:
+            # URLs will just work too
+            path = Path(source).expanduser().resolve()
 
-            path = Path(location).expanduser().resolve()
-
-            # Folder
             if path.is_dir():
                 for item in path.iterdir():
                     yield from cls.factory(item)
+
                 continue
 
-            # File
-            mime, encoding = guess_type(str(path))
+            try:
+                yield cls(path)
 
-            if not encoding and mime and mime.startswith("image/"):
-                try:
-                    yield cls(path)
-                except (BadFileError, OSError) as err:
-                    print(TERM.red(f"\n{err.args[0]}\n"))
+            except (OSError, requests.RequestException) as err:
+                if not ignore_fails:
+                    raise
+
+                if print_fails:
+                    print(TERM.red(err.args[0]))
